ea879885947f449dc0f63557301fda44b4126ecc
fmfahath <fmfahath.dev@gmail.com>
2024-01-08 04:18:19 +0530

    @media(885px) :  menuIcon show/hide function added+

--- app.js
+++ app.js
@@ -1,5 +1,20 @@
 const header = document.querySelector('header');
+const menuIcon = document.querySelector('#menu-icon');
+const  navbar = document.querySelector('.navbar');
 
+
+//navbar scroll bg color channge event
 window.addEventListener('scroll', () => {
     header.classList.toggle('sticky', window.scrollY > 0);
+});
+
+// menu icon show/hide fnnction
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+}
+
+window.addEventListener('scroll', () => {
+    menuIcon.classList.remove('bx-x');
+    navbar.classList.remove('active');
 });
